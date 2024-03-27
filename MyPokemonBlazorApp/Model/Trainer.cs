7afa407e40c1cef656803057a9e724015705ea96
1HappyFire <abdi099q@gmail.com>
2024-03-27 10:36:10 +0000

    Changes

--- MyPokemonBlazorApp/Model/Trainer.cs
+++ MyPokemonBlazorApp/Model/Trainer.cs
@@ -1,9 +1,16 @@
-﻿namespace MyPokemonBlazorApp.Model
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyPokemonBlazorApp.Model
 {
     public class Trainer
     {
+        [Required]
         public int Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
-        public Pokemon Companion { get; set; }
+
+        [Required]
+        public Pokemon Companion { get; set; } 
     }
 }
